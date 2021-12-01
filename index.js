47c466e9284c653a99f70a15fcba1692ff7feeed
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2021-12-02 05:42:44 +0700

    fromscratch

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(bodyParser.json());
+// const app = express();
+// const port = 3000;
 
-app.post("/todos", (req, res) => {
-  res.send("Hello World");
-});
-//===============================
+// import todosRoutes from "./routes/todos.js";
 
-app.use(express.json());
+// //middleware untuk body, parse ke JSON dulu
+// app.use(express.json());
 
-app.post("/api/todos", function (req, res) {
-  res.send("success");
-});
+// app.use("/todos", todosRoutes);
 
-app.listen(3000);
+// app.get("/", (req, res) => {
+//   // res.send("Hello World!");
+//   console.log(req);
+// });
+
+// // ini dipisah beda folder?
+// let todos = {
+//   name: "todos",
+//   rows: [
+//     {
+//       id: 1,
+//       name: "today",
+//       done: true,
+//     },
+//   ],
+// };
+
+// //CREATE to do, method harus post kalau ada body
+// app.post("/todos", (req, res) => {
+//   console.log(req.body);
+//   const { text, done } = req.body;
+//   const id = Math.floor(Math.random() * 100);
+//   todos.rows.push({
+//     id: id,
+//     text: text,
+//     done: done,
+//   });
+//   res.send({
+//     id: id,
+//     success: true,
+//   });
+// });
+
+// app.get("/todos", (req, res) => {
+//   res.send(todos.rows);
+// });
+
+// app.delete("/todos/:id", (req, res) => {
+//   const { id } = req.params;
+//   const index = todos.rows.findIndex((x) => x.id === parseInt(id));
+//   todos.rows.splice(index, 1);
+
+//   res.send({
+//     id: id,
+//     delete: true,
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Listening on port http://localhost:${port}`);
+// });
