21629f9633008e2b749f1509ad572e144f0b59ec
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2021-12-02 08:36:39 +0700

    coba test POST dan masukin agenda ke mocking database arr.push()

--- routes/todos.js
+++ routes/todos.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //remove the string jadi variabel kl disini, as mock database
 const todos = [
   {
-    name: "Bangun Pagi",
+    name: "",
     rows: [
       {
         id: 1,
@@ -18,7 +18,7 @@ const todos = [
 
 //all routes in here are starting with /todos
 router.get("/", (req, res) => {
-  console.log(todos);
+  //   console.log(todos);
   res.send(todos);
 });
 
@@ -26,8 +26,10 @@ router.get("/", (req, res) => {
 //from the client to the server, for the user to be created "as todo list"
 
 router.post("/", (req, res) => {
-  console.log("POST ROUTE REACHED");
-  console.log(req.body);
-  res.send("POST ROUTE REACHED");
+  const todo = req.body;
+  //   console.log("POST ROUTE REACHED");
+  //   console.log(req.body);
+  todos.push(todo);
+  res.send(`Agenda ${todo.name} masuk ke dalam database`);
 });
 export default router;
