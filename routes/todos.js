a3027f8ee9b57d703cfb58af66cdd0193ccaf813
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2021-12-02 17:37:07 +0700

    coba ngikutin data yang sama kayak yg dikelas

--- routes/todos.js
+++ routes/todos.js
@@ -6,34 +6,38 @@ const router = express.Router();
 //remove the string jadi variabel kl disini, as mock database
 let todos = [];
 
-//all routes in here are starting with /todos
-router.get("/", (req, res) => {
-  //   console.log(todos);
-  res.send(todos);
-});
-
 //user send data from app to the server that data being the values in the form todos
 //from the client to the server, for the user to be created "as todo list"
 
 router.post("/", (req, res) => {
-  const todo = req.body;
-  todos.push({ ...todo, Id: uuidv4() });
-  res.send(`Agenda ${todo.name} masuk ke dalam database`);
+  const { text, done } = req.body;
+  const id = Math.floor(Math.random() * 100);
+  todos.rows.push({
+    id: id,
+    text: text,
+    done: done,
+  });
+  res.send({
+    id: id,
+    success: true,
+  });
 });
 
 //retrieve the value of id using callback function?
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const foundTodo = todos.find((todo) => todo.id == id);
-  res.send(foundTodo);
+router.get("/", (req, res) => {
+  res.send(todos);
 });
 
 //menghapus nama Agenda dengan spesific id atau logic nya; id nya 'g sama' dengan dia 'name' dibiarin, tetep di array.
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-  todos = todos.filter((todo) => todo.id != id);
+  const index = todos.rows.findIndex((x) => x.id === parseInt(id));
+  todos.rows.splice(index, 1);
 
-  res.send(`Todo with the id ${id} is deleted from the database`);
+  res.send({
+    id: id,
+    delete: true,
+  });
 });
 
 export default router;
