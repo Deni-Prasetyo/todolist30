3371919c4a75e544467ce65d050cffd3ea69966c
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2021-12-02 18:12:01 +0700

    menyesuaikan requirement assignment 30

--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
 //remove the string jadi variabel kl disini, as mock database
-let todos = [];
+let todos = {
+  name: "todos",
+  rows: [
+    {
+      id: 1,
+      name: "today",
+      done: true,
+    },
+  ],
+};
 
 //user send data from app to the server that data being the values in the form todos
 //from the client to the server, for the user to be created "as todo list"
